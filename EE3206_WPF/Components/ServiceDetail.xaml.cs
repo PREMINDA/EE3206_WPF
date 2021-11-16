c2026b8e61731545c720c736fac8eaf27dff5b2f
PREMINDA <premindabandara0777@gmail.com>
2021-11-16 18:22:00 +0530

    Managment Page

---
+++ EE3206_WPF/Components/ServiceDetail.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EE3206_WPF.Components
+{
+    /// <summary>
+    /// Interaction logic for ServiceDetail.xaml
+    /// </summary>
+    public partial class ServiceDetail : UserControl
+    {
+
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Title.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register("Title", typeof(string), typeof(ServiceDetail), new PropertyMetadata("Title Name"));
+
+
+
+        public string SubTitle
+        {
+            get { return (string)GetValue(SubTitleProperty); }
+            set { SetValue(SubTitleProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for SubTitle.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SubTitleProperty =
+            DependencyProperty.Register("SubTitle", typeof(string), typeof(ServiceDetail), new PropertyMetadata("Subtitle DB"));
+
+
+
+        public string Price
+        {
+            get { return (string)GetValue(PriceProperty); }
+            set { SetValue(PriceProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Price.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PriceProperty =
+            DependencyProperty.Register("Price", typeof(string), typeof(ServiceDetail), new PropertyMetadata("xx"));
+
+        public static readonly RoutedEvent ClickEvent =
+           EventManager.RegisterRoutedEvent(nameof(DeleteListItem), RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(UsersDetail));
+
+        public event RoutedEventHandler DeleteListItem
+        {
+            add { AddHandler(ClickEvent, value); }
+            remove { RemoveHandler(ClickEvent, value); }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            RaiseEvent(new RoutedEventArgs(ClickEvent));
+        }
+
+
+        public ServiceDetail()
+        {
+            InitializeComponent();
+        }
+    }
+}
