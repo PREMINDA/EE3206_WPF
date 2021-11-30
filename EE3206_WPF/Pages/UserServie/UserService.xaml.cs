5b821c1b90746393fbb5b592f24ab968a579e965
PREMINDA <premindabandara0777@gmail.com>
2021-12-01 02:08:51 +0530

    User Servicers UI

---
+++ EE3206_WPF/Pages/UserServie/UserService.xaml.cs
@@ -0,0 +1,36 @@
+﻿using EE3206_WPF.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EE3206_WPF.Pages.UserServie
+{
+    /// <summary>
+    /// Interaction logic for UserService.xaml
+    /// </summary>
+    public partial class UserService : Page
+    {
+        DataBaseRepository repository = new DataBaseRepository();
+        public UserService()
+        {
+            InitializeComponent();
+            load();
+        }
+
+        private void load()
+        {
+            servicedata.ItemsSource = repository.Services.ToList();
+        }
+    }
+}
